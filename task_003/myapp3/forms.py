ef2d281d85ec860354b9e444677bd575f5ed0630
Dmitriy Karol <7052927350@mail.ru>
2023-10-17 15:05:18 +0600

    product photo

--- task_003/myapp3/forms.py
+++ task_003/myapp3/forms.py
@@ -5,3 +5,8 @@ class ProductEditForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'description', 'price', 'photo']
